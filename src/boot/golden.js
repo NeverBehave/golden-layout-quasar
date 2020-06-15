d17a1248e185856842540f7e488058a3172ea3dc
NeverBehave <gayhub@never.pet>
2020-06-15 13:45:45 -0400

    init

---
+++ src/boot/golden.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import vgl from 'vue-golden-layout'
+import 'golden-layout/src/css/goldenlayout-light-theme.css'
+Vue.use(vgl)
---
+++ src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+    <golden-layout class="screen">
+    <gl-row>
+      <gl-component  title="component1">
+        <h1>Component 1</h1>
+      </gl-component>
+      <gl-dstack>
+        <gl-component title="component2">
+          <h1>Component 2</h1>
+        </gl-component>
+        <gl-component title="component3">
+          <h1>Component 3</h1>
+        </gl-component>
+      </gl-dstack>
+    </gl-row>
+  </golden-layout>
+</template>
+
+<style scoped>
+.screen {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
