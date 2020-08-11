c037b8591c0be7b917f6d2be347879c86d18ae18
NeverBehave <gayhub@never.pet>
2020-08-11 12:03:08 -0400

    Merge commit '3f0d9040f924a517619681c24f3e78d4222eede2' as 'deps/vue-golden-layout'

---
+++ deps/vue-golden-layout/src/pages/Index.vue
@@ -0,0 +1,53 @@
+<template>
+    <golden-layout class="screen">
+    <gl-row>
+      <gl-col>
+        <gl-component  title="component1">
+          <h1>Component 1</h1>
+          <button @click="show = true">on</button>
+          <button @click="show = false">off</button>
+          <button @click="clear">cc</button>
+          <input v-model="w"/>
+        </gl-component>
+      </gl-col>
+    <gl-col>
+        <gl-component>
+            <h1>Placeholder here, you will need at least two component left (not dynamic) to avoid error</h1>
+        </gl-component>
+    </gl-col>
+      <gl-col v-if=" show" :width=w @destroy="show = false">
+        <gl-stack>
+          <gl-component title="component2">
+            <h1>Component 2</h1>
+          </gl-component>
+          <gl-component title="component3">
+            <h1>Component 3</h1>
+          </gl-component>
+        </gl-stack>
+      </gl-col>
+    </gl-row>
+  </golden-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: true,
+      w: 98
+    }
+  },
+  methods: {
+    clear () {
+      localStorage.clear()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.screen {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
